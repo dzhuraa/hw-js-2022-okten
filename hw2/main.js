6769c15b8823274c9009f10df899c1346a6812bb
dzhuraa <d.zhurakivsky@gmail.com>
2022-11-03 14:37:08 +0200

    added hw2 + some staff in main.js

---
+++ hw2/main.js
@@ -0,0 +1,120 @@
+
+//Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+// let arr = [1, 5, 3.14, 'kokos', true, false, 'vasya', 'petya', 'kolya', 100500]
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+// console.log(arr[8]);
+// console.log(arr[9]);
+
+//Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+// let kateryna = {
+//     title: 'Катерина',
+//     pageCount: 158,
+//     genre: 'poem'
+// }
+// let hamlet = {
+//     title: 'Гамлет',
+//     pageCount: 238,
+//     genre: 'tragedy, drama'
+// }
+// let romeoAndJuliet = {
+//     title: 'Ромео і Джульєтта',
+//     pageCount: 208,
+//     genre: 'tragedy'
+// }
+
+ //Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+
+// let kateryna = {
+//     title: 'Катерина',
+//     pageCount: 158,
+//     genre: 'poem',
+//     authors: [name = 'Taras Shevchenko', age = 'died']
+// }
+// let hamlet = {
+//     title: 'Гамлет',
+//     pageCount: 238,
+//     genre: 'tragedy, drama',
+//     authors: [name = 'William Shakespeare', age = 'died']
+// }
+// let romeoAndJuliet = {
+//     title: 'Ромео і Джульєтта',
+//     pageCount: 208,
+//     genre: 'tragedy',
+//     authors: [name = 'William Shakespeare', age = 'died']
+// }
+
+//Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+let users = [
+    user1 = {
+        name: 'Vasya',
+        username: 'vasya123',
+        password: 'asdqwe'
+    },
+    user2 = {
+        name: 'Kolya',
+        username: 'kolya123',
+        password: 'asdzxc'
+    },
+    user3 = {
+        name: 'Vanya',
+        username: "van'ka.vstan'ka",
+        password: 'vanya123'
+    },
+    user4 = {
+        name: 'Petya',
+        username: 'petya123',
+        password: 'asd2749qwe'
+    },
+    user5 = {
+        name: 'Kolya',
+        username: 'kolya123',
+        password: 'asdkolyaqwe'
+    },
+    user6 = {
+        name: 'Masha',
+        username: 'masha123',
+        password: 'asdmashaqwe'
+    },
+    user7 = {
+        name: 'Oleh',
+        username: 'oleh123',
+        password: 'asdolehqwe'
+    },
+    user8 = {
+        name: 'Kokos',
+        username: 'kokos123',
+        password: 'asdkokosqwe'
+    },
+    user9 = {
+        name: 'Mykyta',
+        username: 'mykytosuser',
+        password: 'asd235'
+    },
+    user10 = {
+        name: 'Mandaryn',
+        username: 'mandaryn123',
+        password: 'asdqweMan'
+    }
+]
+
+console.log(user1.password);
+console.log(user2.password);
+console.log(user3.password);
+console.log(user4.password);
+console.log(user5.password);
+console.log(user6.password);
+console.log(user7.password);
+console.log(user8.password);
+console.log(user9.password);
+console.log(user10.password);
