e32589e313f153673f55dd92e9b3b2a7e0b9b21f
dzhuraa <d.zhurakivsky@gmail.com>
2022-11-09 14:22:00 +0200

    some changes

--- hw3/main.js
+++ hw3/main.js
@@ -55,7 +55,38 @@
 
 
 // let day = +prompt('Який сьогодні день тижня?')
-//
+
+
+// switch (day){
+//     case 1:
+//         document.write('Monday')
+//         break;
+//     case 2:
+//         document.write('Tuesday')
+//         break;
+//     case 3:
+//         document.write('Wednesday')
+//         break;
+//     case 4:
+//         document.write('Thursday')
+//         break;
+//     case 5:
+//         document.write('Friday')
+//         break;
+//     case 6:
+//         document.write('Saturday')
+//         break;
+//     case 7:
+//         document.write('Sunday')
+//         break;
+//     default:
+//         document.write('Error')
+// }
+
+
+//or
+
+
 // if(day === 1){
 //     document.write('Monday')
 // }else if(day === 2){
@@ -103,3 +134,5 @@
 
 
 
+// let x = +prompt('') || 'default'
+// document.write(x)
